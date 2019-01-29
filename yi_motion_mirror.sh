e19574fbb07bd45b5c379497aad668e9f635a84b
mwaldegg <m.waldegger@gmail.com>
2019-01-29 02:46:31 +0100

    Update

--- yi_motion_mirror.sh
+++ yi_motion_mirror.sh
@@ -5,6 +5,7 @@
 # Add cronjob:
 # @reboot @reboot sleep 60 && /home/pi/MaggoMirrorTools/yi_motion_mirror.sh >> /home/pi/yi_motion_mirror.log 2>&1
 
+
 laststate="off"
 
 while [ 1 -eq 1 ]
@@ -14,11 +15,11 @@ do
 
 	if [ -z "$motion" ]; then
 		echo "No motion detected"
-        if [ $laststate == "on" ]; then
-            echo "Turning off Mirror"
-            wget 'http://1localhost:8080/remote?action=MONITOROFF' -q > /dev/null 2>&1 #Disable MagicMirror
-        fi
-        laststate="off"
+        	if [ $laststate == "on" ]; then
+            		echo "Turning off Mirror"
+           		 wget 'http://1localhost:8080/remote?action=MONITOROFF' -q > /dev/null 2>&1 #Disable MagicMirror
+        	fi
+        	laststate="off"
 		sleep 10
 	else
 		echo "Motion detected: $motion"
