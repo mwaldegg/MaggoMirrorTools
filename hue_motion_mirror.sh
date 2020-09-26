43dc921bdd6111e2787871166606da7a4e88f186
mwaldegg <m.waldegger@gmail.com>
2020-09-26 17:21:31 +0200

    sleeptime config

--- hue_motion_mirror.sh
+++ hue_motion_mirror.sh
@@ -8,6 +8,26 @@
 #clear logfile
 test -f /home/pi/hue_motion_mirror.log && > /home/pi/hue_motion_mirror.log
 
+echo -n "$(date): "
+echo "startup: $0 $@"
+
+ONTIME_DEFAULT=300
+
+#check if alternative ONTIME set in parameter $1
+if [ -z "$1" ]; then
+  ONTIME=$ONTIME_DEFAULT
+else
+  ONTIME=$1
+  echo sleeptime set from call parameter: $ONTIME
+fi
+
+[ -n "$ONTIME" ] && [ "$ONTIME" -eq "$ONTIME" ] 2>/dev/null
+if [ $? -ne 0 ]; then
+   #$ONTIME no valid number / set do default
+   echo "Parameter: $1 is not a valid numer! usinge default ($ONTIME_DEFAULT)"
+   ONTIME=$ONTIME_DEFAULT
+fi
+
 laststate="on"
 
 while [ 1 -eq 1 ]
@@ -22,8 +42,8 @@ while [ 1 -eq 1 ]
       wget -qO- 'http://localhost:8080/remote?action=MONITORON' > /dev/null 2>&1 #Enable MagicMirror
     fi
     laststate="on"
-    echo "Sleeping..."
-    sleep 300
+    echo "Sleeping for $ONTIME sec..."
+    sleep $ONTIME
   else
     echo "No motion detected"
     if [ $laststate == "on" ]; then
